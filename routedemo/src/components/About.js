9a1734617352f2736e6547f851a0db4517a53823
Ankitanshu Kr <ankieduhorizon@gmail.com>
2024-08-26 11:53:19 +0530

    dynamic routing

--- routedemo/src/components/About.js
+++ routedemo/src/components/About.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
-export default function About() {
-  return <div>This Is About Page</div>;
+export default function About(props) {
+  const params = useParams();
+  return <div>This Is About Page | {params?.id}</div>;
 }
--- routedemo/src/components/Navbar.js
+++ routedemo/src/components/Navbar.js
@@ -5,8 +5,8 @@ export default function Navbar() {
   return (
     <div>
       <p>This Is Navbar Page</p>
-      <a href="/">Home</a> {" | "}
-      <a href="/about">About</a>
+      <a href="/">Home</a> |<a href="/about">About</a> |
+      <a href="/about/123">About ID</a>
       <Outlet />
     </div>
   );
